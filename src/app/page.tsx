b2e487b53826cefa8e55fbd52d8b2adfdada8eca
modernscape <modernscape@gmail.com>
2026-05-25 10:28:11 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+
+export default function ContainImage() {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "black", // ここで背景を黒にする
+        zIndex: -1,
+      }}
+    >
+      <Image
+        src="/dynamic-crystals.jpg"
+        alt="dynamic-crystals"
+        fill
+        style={{
+          objectFit: "contain", // 画像全体を表示し、アスペクト比を維持
+        }}
+        priority
+      />
+    </div>
+  )
+}
